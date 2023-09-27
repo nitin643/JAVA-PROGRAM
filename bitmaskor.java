5249ea75032a90eab8b4053b2cf0452aab164823
Nitin Srivastava <nitinsrivaspaa445@gmail.com>
2023-09-27 14:57:35 +0530

    BIT mask

---
+++ bitmaskor.java
@@ -0,0 +1,10 @@
+public class bitmaskor {
+    public static void main(String args[]){
+        int n=5;
+        int pos=3;
+        int bitmask=1<<pos;
+        int newNumber=bitmask | n;
+        System.out.println(newNumber);
+    }
+    
+}
